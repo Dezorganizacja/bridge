5c32585d41cdcb4914529714aab25cf54defd0ef
Larhard <larhard@gmail.com>
2015-03-15 20:24:17 +0100

    [refactor] GUI: Create Controller superclass

--- src/main/java/com/dezorganizacja/bridge/view/graphical/FXApplication.java
+++ src/main/java/com/dezorganizacja/bridge/view/graphical/FXApplication.java
@@ -44,7 +44,7 @@ public class FXApplication extends Application implements Observer {
         stage.setTitle("Bridge");
         stage.setScene(new Scene(root, 300, 275));
 
-        MainController controller = loader.getController();
+        Controller controller = loader.getController();
         if (controller != null) {
             controller.init(mainPresenter);
         }
