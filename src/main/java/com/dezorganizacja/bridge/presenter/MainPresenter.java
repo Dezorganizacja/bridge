e302d1e53277656b0d607edec860ab37c5596a1e
Larhard <larhard@gmail.com>
2015-03-15 18:52:39 +0100

    Add stage to MainPresenter
    identifies stage of program (loading, main_menu, playing_cards etc.)

--- src/main/java/com/dezorganizacja/bridge/presenter/MainPresenter.java
+++ src/main/java/com/dezorganizacja/bridge/presenter/MainPresenter.java
@@ -4,6 +4,7 @@ import com.dezorganizacja.bridge.domain.Message;
 
 public class MainPresenter {
     private final Message message;
+    private String stage;
 
     public MainPresenter(Message message) {
         this.message = message;
@@ -11,5 +12,13 @@ public class MainPresenter {
 
     public Message getMessage() {
         return this.message;
+    }
+
+    public String getStage() {
+        return this.stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
     }
 }
