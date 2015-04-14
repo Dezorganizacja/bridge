e8471565b47a0041d54f9e0da515772680a6e556
MuchomorGrabarz <mz18@o2.pl>
2015-04-15 01:11:51 +0200

    Slight credits fix.

--- src/main/java/com/dezorganizacja/bridge/view/graphical/CreditsController.java
+++ src/main/java/com/dezorganizacja/bridge/view/graphical/CreditsController.java
@@ -3,14 +3,21 @@ package com.dezorganizacja.bridge.view.graphical;
 import com.dezorganizacja.bridge.presenter.MainPresenter;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
+
+import java.io.IOException;
 
 import static java.lang.Thread.sleep;
 
@@ -26,85 +33,34 @@ public class CreditsController extends Controller {
     public void init(MainPresenter presenter) {
         super.init(presenter);
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/movingCredits.fxml"));
+        VBox creditsBox;
+        try {
+            creditsBox = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException("error while loading /fxml/movingCredits.fxml resource", e);
+        }
+
+
         creditsPane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        int duration = 10000;
 
-        VBox myCreditsBox = new VBox();
-        myCreditsBox.getChildren().add((new CreditsNodeController()).setCodePieceLabel("OPs").addAuthor("Grzegorz Świrski").addAuthor("Marek Rusinowski").accept());
-        myCreditsBox.getChildren().add((new CreditsNodeController()).setCodePieceLabel("GUI").addAuthor("Michał Ziobro").addAuthor("Bartłomiej Puget").addAuthor("Dominika Salawa").addAuthor("Maciej Wachulec").accept());
-        myCreditsBox.getChildren().add((new CreditsNodeController()).setCodePieceLabel("TUI").addAuthor("Maciej Woźniak").accept());
 
-        creditsPane.getChildren().add(myCreditsBox);
+        creditsPane.getChildren().add(creditsBox);
 
         Path path = new Path();
-        path.getElements().add(new MoveTo(300,600));
-        path.getElements().add(new LineTo(300,-200));
+        path.getElements().add(new MoveTo(300,creditsPane.getHeight()+creditsBox.getPrefHeight()/2+100)); // sth like window size, dunno if it's fixed or not
+        path.getElements().add(new LineTo(300,-creditsBox.getPrefHeight()/2-100));
+
         PathTransition pathTransition = new PathTransition();
-        pathTransition.setDuration(Duration.millis(duration));
+        pathTransition.setDuration(Duration.millis(20000));
         pathTransition.setPath(path);
         pathTransition.setOrientation(PathTransition.OrientationType.NONE);
-        pathTransition.setNode(myCreditsBox);
-        pathTransition.setCycleCount(Timeline.INDEFINITE);
-        pathTransition.setAutoReverse(true);
+        pathTransition.setNode(creditsBox);
+        pathTransition.setCycleCount(1);
         pathTransition.play();
-        new Thread(() -> {
-            try
-            {
-                sleep(duration);
-            } catch (InterruptedException e) {
-                RuntimeException tmp = new RuntimeException("Interrupted not expected");
-                throw tmp;
-            }
-            pathTransition.stop();
-            presenter.setState("main");
-        }).start();
+        pathTransition.setOnFinished(event -> presenter.setState("main"));
 
-    }
-
-    public static class CreditsNodeController extends VBox {
-
-
-
-        private class AuthorLabel extends Label {
-            public AuthorLabel(String name) {
-                super(name);
-                this.setFont(new Font("Verdena", 20));
-                this.setTextFill(Color.LIGHTGRAY);
-            }
-        }
-
-        private class CodePieceLabel extends Label {
-            public CodePieceLabel(String name) {
-                super(name);
-                this.setFont(new Font("Verdena", 27));
-                this.setTextFill(Color.LIGHTGRAY);
-            }
-        }
-
-        public CodePieceLabel codePieceLabel;
-
-        public CreditsNodeController() {
-            super();
-            codePieceLabel = new CodePieceLabel("Sample piece");
-            this.getChildren().add(codePieceLabel);
-        }
-
-        CreditsNodeController setCodePieceLabel(String name) {
-            codePieceLabel.setText(name);
-            return this;
-        }
-
-        CreditsNodeController addAuthor(String author) {
-            this.getChildren().add(new AuthorLabel(author));
-            return this;
-        }
-
-        CreditsNodeController accept() {
-            Region reg = new Region();
-            reg.setMinHeight(40);
-            this.getChildren().add(reg);
-            return this;
-        }
     }
 }
+
